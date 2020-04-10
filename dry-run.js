7c300f786c0f4f43e1a0a29894f44fd7e3a49355
Hosie <jhosie@uk.ibm.com>
2020-04-10 11:29:21 +0100

    async

--- dry-run.js
+++ dry-run.js
@@ -1,36 +1,39 @@
 const ExifImage = require('exif').ExifImage;
 const targetFilePath = require('./target-file-path.js').targetFilePath
-var recursive = require("recursive-readdir");
+const realRecursive = require("recursive-readdir");
+const util = require('util')
+const recursive =util.promisify(realRecursive)
 
-
-
-
-module.exports.dryRun = function(filepath){
-  console.log(filepath)
-  try {
-    recursive(filepath, function (err, files) {
-      // `files` is an array of file paths
-      console.log(files);
-      files
-      .forEach(file=>{
-        new ExifImage({ image : file }, function (error, exifData) {
-            if (error)
-            {
-              console.log(`Error: with ${file}`+error.message);
-            }
-            else
-            {
-              //console.log(exifData); // Do something with your data!
-              let result = targetFilePath(exifData)
-              console.log(`${file} => ${result}`)
-            }
-        });
-      })
+async function readExif(file){
+  return new Promise((resolve,reject)=>{
+    new ExifImage({ image : file }, function (error, exifData) {
+        if (error)
+        {
+          //console.log(`Error: with ${file}`+error.message);
+          reject(error)
+        }
+        else
+        {
+          //console.log(exifData); // Do something with your data!
+          let result = targetFilePath(exifData)
+          //console.log(`${file} => ${result}`)
+          resolve({source:file,target:result})
+        }
     });
-
-  } catch (error) {
-    console.log('Error: ' + error.message);
-    console.log('Error: ' + error.stack);
-  }
-
+  })
 }
+async function dryRun(filepath){
+
+  console.log(filepath)
+    try {
+      let results = await recursive(filepath)
+      results = results.map(readExif)
+      return Promise.allSettled(results)
+
+    } catch (error) {
+      //console.log('Error: ' + error.message);
+      //console.log('Error: ' + error.stack);
+      throw error
+    }
+}
+module.exports.dryRun = dryRun
--- test/e2e/scripts/dry-run.test.js
+++ test/e2e/scripts/dry-run.test.js
@@ -1,2 +1,7 @@
-const dryRun = require('../../../index.js').dryRun
-dryRun(`${__dirname}/../input`)
+//const dryRun = require('../../../dry-run.js').dryRun
+
+(async () => {
+  const dryRun = require('../../../index.js').dryRun
+  let result = await dryRun(`${__dirname}/../input`)
+  console.log(result)
+})();
