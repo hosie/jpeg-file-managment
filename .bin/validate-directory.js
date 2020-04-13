8f3d526bc4a1d324c0421e8ae03197ead63e7eb9
Hosie <jhosie@uk.ibm.com>
2020-04-13 12:28:54 +0100

    UnhandledPromiseRejectionWarning

--- .bin/validate-directory.js
+++ .bin/validate-directory.js
@@ -3,11 +3,10 @@
   console.log(directoryName)
   const fs = require('fs')
   const validateDirectory = require('../index.js').validateDirectory
-
   let result = await validateDirectory(directoryName)
   if(result !== true){
     fs.writeFileSync('./results.json',JSON.stringify(result,null,2),{encoding:"utf-8"})
-    throw new Error(`result is not true: ${result}`)
+    console.log("there are some invalid files found.  See results.json")
 
   } else {
     console.log("Passed")
