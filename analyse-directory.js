8d8b2e980403c3e116a5d99649b2660d19b1f42a
Hosie <jhosie@uk.ibm.com>
2020-04-13 18:57:02 +0100

    add logs

--- analyse-directory.js
+++ analyse-directory.js
@@ -5,37 +5,51 @@ const realRecursive = require("recursive-readdir");
 const fs = require('fs')
 const util = require('util')
 const path = require('path')
-const recursive =util.promisify(realRecursive)
+const recursive = util.promisify(realRecursive)
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({
+  name: "analyse-directory",
+  streams: [{
+    path: './bunyan.log',
+    // `type: 'file'` is implied
+  }]
+});
 
 
-async function analyseDirectory(dirpath){
-  if(! fs.existsSync(dirpath)){
+async function analyseDirectory(dirpath) {
+  if (!fs.existsSync(dirpath)) {
     return Promise.reject(`${dirpath} does not exist`)
 
   }
   const stats = fs.statSync(dirpath)
 
-  if(! stats.isDirectory() ){
+  if (!stats.isDirectory()) {
     return Promise.reject(`${dirpath} is not a directory`)
   }
 
-  async function readExif(file){
-    return new Promise((resolve,reject)=>{
-      new ExifImage({ image : file }, function (error, exifData) {
-          if (error)
-          {
-            reject(error)
+  async function readExif(file) {
+    log.info(`#readExif file`);
+    return new Promise((resolve, reject) => {
+      new ExifImage({
+        image: file
+      }, function(error, exifData) {
+        if (error) {
+          reject(error)
+        } else {
+          let filePath = targetFilePath(exifData)
+          let fileName = targetFileName(exifData)
+          if (filePath === null || fileName === null) {
+            reject({
+              source: `File: ${file}`,
+              code: `NO_DATE_TIME`
+            })
+          } else {
+            resolve({
+              source: file,
+              target: path.join(filePath, fileName)
+            })
           }
-          else
-          {
-            let filePath = targetFilePath(exifData)
-            let fileName = targetFileName(exifData)
-            if(filePath===null || fileName===null){
-              reject({source:`File: ${file}`,code:`NO_DATE_TIME`})
-            }else{
-              resolve({source:file,target:path.join(filePath,fileName)})
-            }
-          }
+        }
       });
     })
   }
